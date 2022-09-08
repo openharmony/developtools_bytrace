29b7306cee8d65e0f51b2efc2ced4b93d8ec20c6
liuwei <liuwei573@huawei.com>
2022-09-08 14:27:19 +0800

    Modify the output file path in the scrip
    Signed-off-by: liuwei <liuwei573@huawei.com>

--- script/bytrace_capture.bat
+++ script/bytrace_capture.bat
@@ -16,6 +16,6 @@ hdc_std shell "echo > /sys/kernel/debug/tracing/trace"
 hdc_std shell "echo 4096 > /sys/kernel/debug/tracing/saved_cmdlines_size"
 hdc_std shell "bytrace -t 10 -b 204800 --overwrite filemanagement gresource devicemanager deviceprofile dscreen dinput dhfwk accessibility dsched samgr ohos ace workq idle app ark ability binder disk distributeddatamgr dsoftbus freq graphic irq mdfs memory memreclaim misc mmc msdp multimodalinput notification pagecache regulators rpc sched sync window zaudio zcamera zimage zmedia > /data/local/tmp/data.ftrace"
 hdc_std shell "echo > /sys/kernel/debug/tracing/trace"
-hdc_std shell "sed -i '1,2d' /data/mynewtrace.ftrace"
-hdc_std file recv /data/mynewtrace.ftrace .
+hdc_std shell "sed -i '1,2d' /data/local/tmp/mynewtrace.ftrace"
+hdc_std file recv /data/local/tmp/mynewtrace.ftrace .
 pause
